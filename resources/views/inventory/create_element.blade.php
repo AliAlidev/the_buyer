c647bf4e19db1cf4e8f1f34997095a9f0453b71b
ali ali <ali.ali.deve@gmail.com>
2022-07-27 16:05:40 +0300

    fix

--- resources/views/inventory/create_element.blade.php
+++ resources/views/inventory/create_element.blade.php
@@ -68,7 +68,7 @@
                                                 <div class="col-md-2"></div>
                                                 <div id="barcode-result" class="result-text">&nbsp;</div>
                                                 <div class="col-md-8">
-                                                    <scandit-barcode-picker id="barcode-picker" class="scanner" style="width: 100%; height: 80%;"
+                                                    <scandit-barcode-picker id="barcode-picker" class="scanner" style="width: 100%; height: 50%;"
                                                         configure.licenseKey="{{ config('services.bar_code_key') }}"
                                                         configure.engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build/"
                                                         accessCamera="false" visible="false" playSoundOnScan="true"
