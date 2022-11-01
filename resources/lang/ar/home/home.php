6c1f4a7f7f131bbedddffe380e98574d40081a6a
ali ali <ali.ali.deve@gmail.com>
2022-11-01 19:05:29 +0300

    fix

--- resources/lang/ar/home/home.php
+++ resources/lang/ar/home/home.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'orders' => 'الطلبات',
+    'orders' => 'الفواتير',
     'orders_count' => 'العدد',
     'orders_amount' => 'إجمالي المبلغ',
-    'sell_orders' => 'طلبات المبيع',
+    'sell_orders' => 'فواتير المبيع',
     'sell_orders_count' => 'العدد',
     'sell_orders_amount' => 'إجمالي المبلغ',
-    'buy_orders' => 'طلبات الشراء',
+    'buy_orders' => 'فواتير الشراء',
     'buy_orders_count' => 'العدد',
     'buy_orders_amount' => 'إجمالي المبلغ',
     'sale_report' => 'مخطط مبيعات شهري',
